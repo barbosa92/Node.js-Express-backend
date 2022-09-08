91837ddc86ef04730ac383f29fe1b9fe4a775300
barbosa92 <alexrp_92@hotmail.com>
2022-09-08 15:37:48 +0000

    GET, DELETE & POST endpoints implemented

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         "id":1,
@@ -40,12 +42,35 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    console.log({id})
-    
-    const note = notes.find(notes => notes.id == id)
-    console.log({note})
-    response.json(note)
+    const note = notes.find(note => note.id == id)
+    {note ? response.json(note) : response.status(404).end()}
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id  = Number(request.params.id)
+    notes = notes.filter(note => note.id != id)
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if (!note || !note.content) {
+        return response.status(400).json({error: "note.content is missing"})
+    }
+
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+    const newNote = {
+        "id": maxId + 1,
+        "content": note.content,
+        "date": new Date().toISOString(),
+        "important": typeof note.important != "undefined" ? note.important : false
+    }
+    notes = [...notes, newNote]
+    response.json(newNote)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
